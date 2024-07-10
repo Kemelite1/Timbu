1923c45d480703fca3e39fea949c0729a2dc08b1
kemelite1 <osikemelite42@gmail.com>
2024-07-10 16:35:39 +0100

    timbu shop

---
+++ timbushop/src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import ProductDetail from './components/ProductDetail';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/productspage" element={<ProductsPage />} />
+      <Route path="/productdetail" element={<ProductDetailPage />} />
+      <Route path="/product-detail/:productId" element={<ProductDetail />} />
+
+
+    </Route>
+  )
+);
+
+
+const App = () => {
+  return (
+    <RouterProvider router={router} />
+
+  );
+}
+
+export default App
